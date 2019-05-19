622a9195eca0039cf909ca7ca86eeb9c89272852
Takashi Aoki | 隆 青木 <Sir.Creeper@yandex.ru>
2019-05-19 13:43:31 +0500

    Add files via upload

---
+++ AntiFlashInternal/AntiFlashInternal/dllmain.cpp
@@ -0,0 +1,44 @@
+#include "Offsets.h"
+
+bool IsWindowFocused()
+{
+	HWND tWnd = FindWindow(NULL, "Counter-Strike: Global Offensive");
+
+	if (GetForegroundWindow() == tWnd)
+		return true;
+	else
+		return false;
+}
+
+void AntiFlash()
+{
+	int FlashDuration = *(int*)(value.LocalPlayer + offset.m_flFlashDuration);
+	if (FlashDuration > 0)
+		*(int*)(value.LocalPlayer + offset.m_flFlashDuration) = 0;
+}
+
+BOOL WINAPI main()
+{
+	value.LocalPlayer = *(DWORD*)(GameModule + offset.dwLocalPlayer);
+	if (!value.LocalPlayer)
+		while (!value.LocalPlayer)
+			value.LocalPlayer = *(DWORD*)(GameModule + offset.dwLocalPlayer);
+
+	while (true)
+	{
+		if (!IsWindowFocused())
+			continue;
+
+		AntiFlash();
+	}
+}
+
+BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+	if (dwReason == DLL_PROCESS_ATTACH) {
+		DisableThreadLibraryCalls(hModule);
+		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)main, NULL, NULL, NULL);
+	}
+	return TRUE;
+}
+
