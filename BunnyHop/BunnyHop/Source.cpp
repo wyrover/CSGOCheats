f6b3c9f21210e839533483e1fe280fa3aec89ae1
Takashi Aoki | 隆 青木 <Sir.Creeper@yandex.ru>
2019-05-20 21:09:35 +0500

    Update Source.cpp

--- BunnyHop/BunnyHop/Source.cpp
+++ BunnyHop/BunnyHop/Source.cpp
@@ -18,17 +18,15 @@ int main()
 	while (!mem.Attach("csgo.exe", PROCESS_ALL_ACCESS)) {}
 	GameModule = mem.GetModule("client_panorama.dll");
 
-	value.LocalPlayer = mem.Read<DWORD>(GameModule.dwBase + offset.dwLocalPlayer);
-
-	if (!value.LocalPlayer)
-		while (!value.LocalPlayer)
-			value.LocalPlayer = mem.Read<DWORD>(GameModule.dwBase + offset.dwLocalPlayer);
-
 	while (true)
 	{
 		if (!IsWindowFocused())
 			continue;
 
+		value.LocalPlayer = mem.Read<DWORD>(GameModule.dwBase + offset.dwLocalPlayer);
+		if (!value.LocalPlayer)
+			continue;
+		
 		BunnyHop();
 
 		Sleep(1);
